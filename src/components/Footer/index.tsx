3b6591e367b42f81322ef3ea744dbea53e6f102e
Hashmat Abbas <hashmat@graffitecs.com>
2019-08-26 16:44:03 +0500

    Rewritten footer as a class component to show the same UI of webPage

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
-const Footer = (props: any) => {
-  return (
-    <div
-      style={{
-        marginTop: 210,
-        padding: 24,
-        backgroundColor: 'white',
-        display: 'flex'
-      }}
-    >
-      <Typography variant="h6">Footer</Typography>
-      <Typography variant="h6" style={{ flexGrow: 1, textAlign: 'right' }}>
-        Copyrights@ xyz, 2019 INC.
-      </Typography>
-    </div>
-  );
-};
+class Footer extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          marginTop: 210,
+          padding: 24,
+          backgroundColor: 'white',
+          display: 'flex'
+        }}
+      >
+        <Typography variant="h6">Footer</Typography>
+        <Typography variant="h6" style={{ flexGrow: 1, textAlign: 'right' }}>
+          Copyrights@ xyz, 2019 INC.
+        </Typography>
+      </div>
+    );
+  }
+}
+
 export default Footer;
