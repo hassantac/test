04443d66cc8d7d17f6b0fee7940ddbd8827f0aa6
Hashmat Abbas <hashmat@graffitecs.com>
2019-08-26 16:30:12 +0500

    Added Page

---
+++ src/Pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+const MainPage = (props: any) => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default MainPage;
