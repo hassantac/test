8b4b4f33b872af8750052fd8b978310edfef4720
Hashmat Abbas <hashmat@graffitecs.com>
2019-08-27 12:13:54 +0500

    Added point 11, Reimplemented HOC with ReactContext

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,27 @@ import Purchases from './Pages/Purchases';
 import FormPage from './Pages/FormPage';
 import TwoBoxesPage from './Pages/TwoBoxesPage';
 import HOCPage from './Pages/HOCPage';
+import { data } from './components/WithDataHOC/data';
+import { IDataStorageState } from './interfaces';
+export const DataContext = React.createContext<IDataStorageState[]>(data);
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Route exact path="/0" component={MainPage} />
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/1" render={() => <NavBar content={BoxPage} />} />
-      <Route exact path="/2" render={() => <NavBar content={Purchases} />} />
-      <Route exact path="/3" render={() => <NavBar content={FormPage} />} />
-      <Route exact path="/4" render={() => <NavBar content={TwoBoxesPage} />} />
-      <Route exact path="/5" render={() => <NavBar content={HOCPage} />} />
-    </Router>
+    <DataContext.Provider value={data}>
+      <Router>
+        <Route exact path="/0" component={MainPage} />
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/1" render={() => <NavBar content={BoxPage} />} />
+        <Route exact path="/2" render={() => <NavBar content={Purchases} />} />
+        <Route exact path="/3" render={() => <NavBar content={FormPage} />} />
+        <Route
+          exact
+          path="/4"
+          render={() => <NavBar content={TwoBoxesPage} />}
+        />
+        <Route exact path="/5" render={() => <NavBar content={HOCPage} />} />
+      </Router>
+    </DataContext.Provider>
   );
 };
 
--- src/components/WithDataHOC/withData.tsx
+++ src/components/WithDataHOC/withData.tsx
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { data } from './data';
 import { IDataHOCState } from '../../interfaces';
+import { DataContext } from '../../App';
 
 function withData(WrappedComponent: any) {
-  return class extends Component<{}, IDataHOCState> {
-    constructor(props: any) {
-      super(props);
-      this.state = {
-        data: []
-      };
-    }
+  return class extends Component {
+    // constructor(props: any) {
+    //   super(props);
+    //   this.state = {
+    //     data: []
+    //   };
+    // }
 
-    componentDidMount() {
-      this.setState({ data });
-    }
+    // componentDidMount() {
+    //   this.setState({ data });
+    // }
+
     render() {
-      const { data } = this.state;
-      return <WrappedComponent data={data} {...this.props} />;
+      // const { data } = this.state;
+      return (
+        <DataContext.Consumer>
+          {props => <WrappedComponent data={props} />}
+        </DataContext.Consumer>
+      );
     }
   };
 }
